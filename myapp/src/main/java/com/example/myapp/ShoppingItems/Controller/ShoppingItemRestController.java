95c3601fc390d25ec49a49536fc92a17a99818da
Michał Moskała <michal.moskala@outlook.com>
2018-03-29 22:45:55 +0200

    controller-corrected-2

--- myapp/src/main/java/com/example/myapp/ShoppingItems/Controller/ShoppingItemRestController.java
+++ myapp/src/main/java/com/example/myapp/ShoppingItems/Controller/ShoppingItemRestController.java
@@ -1,4 +1,4 @@
-package com.pwr.shopping.shoppingitems.controller;
+package com.example.myapp.ShoppingItems.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
