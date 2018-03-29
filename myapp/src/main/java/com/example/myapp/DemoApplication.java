94597449dcac1e56070116784482464e2caf8c94
Michał Moskała <michal.moskala@outlook.com>
2018-03-29 20:31:42 +0200

    -

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World!";
+      return "Sirotkin jest lepszy od kubicy";
     }
 
     public static void main(String[] args) {
