3ff8e789dfaccdc9914aa9f927e503d227ff7001
Michał Moskała <michal.moskala@outlook.com>
2018-03-29 22:36:35 +0200

    controller

---
+++ myapp/src/main/java/com/example/myapp/ShoppingItems/ShoppingItemRestController.java
@@ -0,0 +1,18 @@
+package com.pwr.shopping.shoppingitems.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/")
+public class ShoppingItemRestController {
+
+
+    @RequestMapping(path="/greeting",method= RequestMethod.GET)
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+}
