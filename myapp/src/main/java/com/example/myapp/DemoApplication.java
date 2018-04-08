e60c45228e20bdc93fe49aff74dba70f4c2fdb0e
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 17:55:11 +0200

    usersapi16

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -3,10 +3,12 @@ package com.example.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
 
