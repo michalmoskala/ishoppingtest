bbfe66c9e10cdbe4ee5b888d22c258a1d683daf6
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 16:23:12 +0200

    usersapi6

--- myapp/src/main/java/com/example/myapp/Users/Controller/UsersRestController.java
+++ myapp/src/main/java/com/example/myapp/Users/Controller/UsersRestController.java
@@ -1,34 +1,34 @@
-package com.example.myapp.Users.Controller;
-
-import java.util.concurrent.atomic.AtomicLong;
-
-import com.example.myapp.Greeting;
-import com.example.myapp.Users.User;
-import com.sun.corba.se.spi.presentation.rmi.IDLNameTranslator;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UsersRestController {
-
-    @GetMapping("/users-javaconfig")
-    public Greeting usersWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    private static final String template = "Henlo, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    //@CrossOrigin(origins = "https://ishopping-beta.firebaseapp.com")
-    @GetMapping("/users")
-    public User user(@RequestParam(required=true) int id) {
-        System.out.println("==== in greeting ====");
-        return new User(id,"mihu","m@m.c","admin1");
-    }
-
-}
-
-
+//package com.example.myapp.Users.Controller;
+//
+//import java.util.concurrent.atomic.AtomicLong;
+//
+//import com.example.myapp.Greeting;
+//import com.example.myapp.Users.User;
+//import com.sun.corba.se.spi.presentation.rmi.IDLNameTranslator;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class UsersRestController {
+//
+//    @GetMapping("/users-javaconfig")
+//    public Greeting usersWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+//        System.out.println("==== in greeting ====");
+//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+//    }
+//
+//    private static final String template = "Henlo, %s!";
+//    private final AtomicLong counter = new AtomicLong();
+//
+//    //@CrossOrigin(origins = "https://ishopping-beta.firebaseapp.com")
+//    @GetMapping("/users")
+//    public User user(@RequestParam(required=true) int id) {
+//        System.out.println("==== in greeting ====");
+//        return new User(id,"mihu","m@m.c","admin1");
+//    }
+//
+//}
+//
+//
