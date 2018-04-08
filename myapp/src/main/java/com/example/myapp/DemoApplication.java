85f49d394bb3836c98ba6cb00acac2be0c75498b
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 19:27:44 +0200

    greetingapi

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -2,9 +2,16 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @GetMapping("/api")
+    public String greeting(){
+        return "test";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
