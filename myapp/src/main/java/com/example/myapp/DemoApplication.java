d5cd6bba7664763541f98bd39660f2212c6ffdb7
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 16:24:11 +0200

    usersapi7

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class DemoApplication {
 
 
