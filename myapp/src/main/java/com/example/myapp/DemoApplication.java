557ad6c527eef8343edf07a4dab96cc2bfbb00b9
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 18:06:50 +0200

    usersapi17

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -8,22 +8,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 @SpringBootApplication
 public class DemoApplication {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                .allowedOrigins("*");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/*")
+//                .allowedOrigins("*");
+//            }
+//        };
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
