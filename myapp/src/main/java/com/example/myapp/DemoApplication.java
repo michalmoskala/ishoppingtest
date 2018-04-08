17702b040970882e9e3e5b19c83ee0b036d5133f
Michał Moskała <michal.moskala@outlook.com>
2018-04-08 19:29:16 +0200

    greetingapi2

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
